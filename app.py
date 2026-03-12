bcf96534ece2207e0377261206a09ffd68b59940
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-12 18:54:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import os
+from model import recommend
+
+st.title("🎬 AI Movie Recommendation System")
+
+# Get base folder
+BASE_DIR = os.path.dirname(__file__)
+
+@st.cache_data
+def load_movies():
+    movies = pd.read_csv(os.path.join(BASE_DIR,"dataset/movies.csv"))
+    movies = movies.rename(columns={'Movie Name':'title','Genre':'genres'})
+    return movies
+movies = load_movies()
+selected_movie = st.selectbox("Select a movie", movies['title'].values)
+
+if st.button("Recommend"):
+    names = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    for i, col in enumerate([col1, col2, col3, col4, col5]):
+        if i < len(names):
+            col.write(names[i])
