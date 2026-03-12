00295a1c6b25e87383896913f4c0740c45611b2c
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-12 18:55:53 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+# Load dataset
+movies = pd.read_csv(os.path.join(BASE_DIR,"dataset/movies.csv"))
+
+movies = movies.rename(columns={'Movie Name':'title','Genre':'genres'})
+
+for col in ['overview','keywords']:
+    if col not in movies.columns:
+        movies[col] = ''
+
+movies['tags'] = movies['overview'] + " " + movies['genres'] + " " + movies['keywords']
+
+# Create TF-IDF vectors
+vectorizer = TfidfVectorizer(max_features=3000, stop_words='english')
+vectors = vectorizer.fit_transform(movies['tags'])
+
+def recommend(movie):
+    
+    try:
+        idx = movies[movies['title'] == movie].index[0]
+    except:
+        return ["Movie not found"]*5
+
+    movie_vector = vectors[idx]
+    similarity = cosine_similarity(movie_vector, vectors).flatten()
+
+    recommended_idx = similarity.argsort()[-6:-1][::-1]
+
+    return [movies.iloc[i].title for i in recommended_idx]
